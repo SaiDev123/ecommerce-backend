f6d5f3ca2849a247f1272fb4cfc3dfe089fcd7d9
sai <sai@digital-lync.com>
2021-02-18 07:35:30 +0530

    added product sorting

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,8 @@
 
 var ProductModel= require('../models/product.model');
 
+var url= require('url');
+
 exports.addProduct=function(request,response){
 
     var product= request.body;
@@ -58,4 +60,30 @@ exports.updateProduct=function(request,response){
             response.send(res);
         }
     })
+}
+
+exports.getProductsByChoice= function(request,response){
+
+    var choice=request.query.choice;
+
+     switch(choice){
+        case "asc": ProductModel.find({}).sort({'price':1}).exec((err,docs) => {
+            console.log(docs);
+            response.send(docs);
+        })
+        break;
+        case "desc": ProductModel.find({}).sort({'price':-1}).exec((err,docs) => {
+            console.log(docs);
+            response.send(docs);
+        })
+        break;
+     }
+}
+
+exports.getProductsByFilter=function(request,response){
+
+    var choice=request.query.choice;
+
+     
+
 }
--- routes/product.route.js
+++ routes/product.route.js
@@ -8,5 +8,7 @@ productRouter.post('/addProduct',ProductController.addProduct);
 productRouter.get('/allProducts',ProductController.getProducts);
 productRouter.get('/product/:id',ProductController.getProductById);
 productRouter.put('/updateProduct/:id',ProductController.updateProduct);
+productRouter.get('/getProductsByChoice',ProductController.getProductsByChoice);
+productRouter.get('/getProductsByFilter',ProductController.getProductsByFilter);
 
 module.exports=productRouter;
